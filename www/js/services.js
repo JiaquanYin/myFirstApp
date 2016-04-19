b5695d130e0f20ca481f8d3ee28080f378fd9d74
JiaquanYin <zhu_yin2013@163.com>
2016-04-19 16:01:00 +0800

    ionic tab project & cordova camera plugin

--- www/js/services.js
+++ www/js/services.js
@@ -5,34 +5,117 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      lastTime:'11:07',
+      face: 'img/ben.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      lastTime:'09:07',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      lastTime:'10:07',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      lastTime:'07:07',
+      face: 'img/perry.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      lastTime:'09:07',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      lastTime:'10:07',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      lastTime:'07:07',
+      face: 'img/perry.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      lastTime:'09:07',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      lastTime:'10:07',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      lastTime:'07:07',
+      face: 'img/perry.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      lastTime:'09:07',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      lastTime:'10:07',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      lastTime:'07:07',
+      face: 'img/perry.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      lastTime:'09:07',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      lastTime:'10:07',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      lastTime:'07:07',
+      face: 'img/perry.png'
+    }
+  ];
 
   return {
     all: function() {
+      return chats;
+    },
+    refresh: function() {
+      var chat = {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        lastTime:'08:07',
+        face: 'img/mike.png'
+      };
+      chats.push(chat);
       return chats;
     },
     remove: function(chat) {
@@ -47,4 +130,21 @@ angular.module('starter.services', [])
       return null;
     }
   };
-});
+})
+.factory('Camera',['$q', function($q) {
+  return {
+    getPicture: function(options) {
+      var q = $q.defer();
+      navigator.camera.getPicture(
+        function(result) {
+          q.resolve(result);
+        },
+        function(err) {
+          q.reject(err);
+        },
+        options);
+      return q.promise;
+    }
+  }
+
+}]);
